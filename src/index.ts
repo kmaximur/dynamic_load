102f87271a7cc0cf87837886f599f3acd903938b
kmaximur <ko.max.ur@gmail.com>
2023-11-09 18:48:15 +0700

    -

--- src/index.ts
+++ src/index.ts
@@ -4,27 +4,35 @@ import {strategy} from "./interfaces";
 
 const strategies:strategy[] = []
 
-const dir = path.resolve(__dirname, 'strategies')
+const dir = path.resolve(require!.main!.path, '..','dist', 'strategies')
 
 function execShellCommand(cmd) {
     const exec = require('child_process').exec;
     return new Promise((resolve, reject) => {
         exec(cmd, { cwd: require!.main!.path }, (error, stdout, stderr) => {
-            if (error) console.warn(error);
+            if (error) reject(error)
             resolve(stdout? stdout : stderr);
         });
     });
 }
 
-async function refreshStrategies(){
-    if(!fs.existsSync(dir)) fs.mkdirSync(dir)
-    await execShellCommand('tsc')
+async function refreshStrategies():Promise<boolean>{
+    try {
+        await execShellCommand('tsc')
+    } catch (e){
+        console.log('error tsc build')
+        return false
+    }
+    if(!fs.existsSync(dir)) {
+        console.log('strategy folder not exist')
+        return false
+    }
     const files= fs.readdirSync(dir);
     if(files.length){
         strategies.splice(0,strategies.length)
         for (let i =0; i<files.length; i++){
             const name = dir + '/' + files[i];
-            if (!fs.statSync(name).isDirectory()){
+            if (!fs.statSync(name).isDirectory() && path.extname(files[i]) == '.js'){
                 try {
                     let a = await import(name);
                     strategies.push(a.def)
@@ -34,7 +42,10 @@ async function refreshStrategies(){
             }
         }
     }
-    strategies[0].onBar()
+    // strategies[0].onBar()
+    return true
 }
 
-refreshStrategies()
+refreshStrategies().then((result)=>{
+    console.log(result)
+})
